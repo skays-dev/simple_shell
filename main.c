79ea568cf97a3ddb2c594d7165c4e8657866c9e7
aymane taoufyq <skays.dev@gmail.com>
2023-09-12 14:34:26 +0100

    help

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/** 
+ * main - Simple shell main function
+ * @ac: Count of arguments
+ * @av: Arguments
+ * Return: 0 Always (success)
+*/
+int main(int ac, char **argv)
+{
+    char *line = NULL;
+    /*char **command = NULL;*/
+    int status = 0;
+    (void) ac;
+    (void) argv;
+
+    while (1)
+    {
+        line = read_line();
+        if(line == NULL) 
+            return (0);
+
+        printf("%s\n", line);
+        /* command = splicer(line);
+
+        status = _execute(command, argv); */
+    }
+}
---
+++ readline.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+#include <stdio.h>
+
+char *read_line(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t n;
+
+    write(STDOUT_FILENO, "$ ", 2);
+    n = getline(&line, &len, stdin);
+
+    if(n == -1) {
+        free(line);
+        return (NULL);
+    }
+
+    return (line);
+}
