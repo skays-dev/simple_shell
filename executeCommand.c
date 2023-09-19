01ac7ae62f6723765c964039becc5490d872cfdf
aymane taoufyq <skays.dev@gmail.com>
2023-09-19 14:51:56 +0100

    go

--- executeCommand.c
+++ executeCommand.c
@@ -8,21 +8,25 @@
  */
 int executeCommand(char **command, char **programName)
 {
-    pid_t childPid;
-    int childStatus;
+pid_t childPid;
+int childStatus;
 
-    childPid = fork();
+childPid = fork();
 
-    if (childPid == 0) {
-        if (execve(command[0], command, environ) == -1) {
-            perror(programName[0]);
-            freeStringArray(command);
-            exit(0);
-        }
-    } else {
-        waitpid(childPid, &childStatus, 0);
-        freeStringArray(command);
-    }
+if (childPid == 0)
+{
+if (execve(command[0], command, environ) == -1)
+{
+perror(programName[0]);
+freeStringArray(command);
+exit(0);
+}
+}
+else
+{
+waitpid(childPid, &childStatus, 0);
+freeStringArray(command);
+}
 
-    return (WEXITSTATUS(childStatus));
+return (WEXITSTATUS(childStatus));
 }
