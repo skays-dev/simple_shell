c6decb43e6d55f156cf972d49a8daf154064106a
aymane taoufyq <skays.dev@gmail.com>
2023-09-19 15:09:01 +0100

    go

--- tokenizeInput.c
+++ tokenizeInput.c
@@ -8,57 +8,40 @@
 */
 char **tokenizeInput(char *input)
 {
-char *token = NULL;
-char *temp = NULL;
-char delimiters[] = " \t\n";
-char **commandArray = NULL;
+char *token = NULL, *temp = NULL, delimiters[] = " \t\n", **commandArray = NULL;
 int tokenCount = 0, index = 0;
-
 if (!input)
 {
 return (NULL);
 }
-
 temp = my_strdup(input);
 token = strtok(temp, delimiters);
-
 if (token == NULL)
 {
 free(input), input = NULL;
 free(temp), temp = NULL;
 return (NULL);
 }
-
 while (token)
 {
 tokenCount++;
 token = strtok(NULL, delimiters);
 }
-
-free(temp);
-temp = NULL;
-
+free(temp), temp = NULL;
 commandArray = (char **)malloc(sizeof(char *) * (tokenCount + 1));
-
 if (!commandArray)
 {
-free(input);
-input = NULL;
+free(input), input = NULL;
 return (NULL);
 }
-
 token = strtok(input, delimiters);
-
 while (token)
 {
 commandArray[index] = my_strdup(token);
 token = strtok(NULL, delimiters);
 index++;
 }
-
-free(input);
-input = NULL;
+free(input), input = NULL;
 commandArray[index] = NULL;
-
 return (commandArray);
 }
